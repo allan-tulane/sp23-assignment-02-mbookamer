199212cb05372cfa1b6ca9d48240c7fdc06a1a4d
mbookamer <79340162+mbookamer@users.noreply.github.com>
2023-03-02 11:33:48 -0600

    Update main.py
    implemented the subquadratic_multiply function, commented it all out, and added 2 additional test cases

--- main.py
+++ main.py
@@ -47,12 +47,66 @@ def pad(x,y):
 
 def subquadratic_multiply(x, y):
     ### TODO
+    x = x.binary_vec
+    y = y.binary_vec #we need to turn x and y into vectors so we can make sure the lengths of the binary numbers are equal for multiplication
+    
+    pad(x,y) #want to get the same length of binary integers
+    
+    if len(x) == len(y) == 1: #the most simple base case
+        return x*y #if both have a length 1, then you simply need to multiply the two as normal integers 
+    else:
+        #we want to split both vectors into 2 sections of equal length and return the integer value so that we can call our function again
+        #we want to distinguish between the 2 values created for each variable x and y so we access the vector corresponding to left and right
+        x_left = split_number(x)[0]
+        x_right = split_number(x)[1]
+        y_left = split_number(y)[0]
+        y_right = split_number(y)[1]
+
+        #now we want to change these numbers back into binary form 
+        x_left = BinaryNumber(x_left)
+        x_right = BinaryNumber(x_right)
+        y_left = BinaryNumber(y_left)
+        y_right = BinaryNumber(y_right)
+        
+        #we want to create a variable that is the sum of each individual variable so we can multiply these values together
+        #we then want to convert these to binary numbers like the other parts of our vectors 
+        x_left_right = x_left + x_right
+        x_left_right = BinaryNumber(x_left_right)
+        y_left_right = y_left + y_right
+        y_left_right = BinaryNumber(y_left_right)
+        
+        #we now want to call our function 
+        m = subquadratic_multiply(x_left, y_left)
+        n = subquadratic_multiply(x_right, y_right)
+        o = subquadratic_multiply(x_left_right, y_left_right)
+        o_sum = o - n - m #we want this because o is double counting and we just want the difference WITHOUT the double counts of values
+        
+        #we want to convert them into binary numbers as before
+        m = BinaryNumber(m)
+        n = BinaryNumber(n)
+        o = BinaryNumber(o)
+        #now we want them to be vectors to bit-shift them
+        m = m.binary_vec
+        m = bit_shift(m, len(x))
+        n = n.binary_vec
+        n = bit_shift(n, len(x))
+        o = o.binary_vec
+        o = bit_shift(o, len(x)//2)
+        
+        #now we want to convert the numbers back to integers, sum each individual operation, and return this sum
+        m = binary2int(m).decimal_val
+        n = binary2int(n).decimal_val
+        o = binary2int(o).decimal_val
+        sum = m + n + o
+        return sum
     pass
     ###
 
 ## Feel free to add your own tests here.
 def test_multiply():
     assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(0), BinaryNumber(2)) == 0
+    assert subquadratic_multiply(BinaryNumber(20), BinaryNumber(20)) == 400
 
 def time_multiply(x, y, f):
     start = time.time()
